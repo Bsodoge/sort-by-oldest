b9256fdd9c7a80fe43eec09face21ea5c65f189a
bsodoge <kashhh4u@gmail.com>
2025-01-24 19:10:55 +0000

    api

---
+++ app/api/getComments/route.ts
@@ -0,0 +1,10 @@
+export const dynamic = 'force-static'
+ 
+export async function GET() {
+  const res = await fetch('https://data.mongodb-api.com/...', {
+    
+  })
+  const data = await res.json()
+ 
+  return Response.json({ data })
+}
