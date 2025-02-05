41785ff6f1e0e9a5ae6d78b0e7a4a233b7fcc10e
bsodoge <kashhh4u@gmail.com>
2025-02-05 14:51:23 +0000

    Fix the img problem

--- app/components/Comment.tsx
+++ app/components/Comment.tsx
@@ -1,7 +1,9 @@
 export default function Comment({ content, publishedAt, author, likes, profileImage }: any) {
     return(
-        <div className="flex gap-8 w-1/2 border p-2">
-            <img src={profileImage} alt="No Profile Found" className="aspect-square" />
+        <div className="flex gap-8 w-1/2 border p-4">
+            <div>
+                <img src={profileImage} alt="No Profile Found" className="h-24 max-w-24"/>
+            </div>
             <div className="flex flex-col gap-4">
                 <div className="flex">
                     <div className="">{ author }</div>
--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           <h2>Find comments from a <em className="italic">long</em> time ago...</h2>
         </div>
         <Form setComments={setComments}></Form>
-        <div className="flex flex-col gap-16 p-10 justify-center items-center">
+        <div className="flex flex-col gap-16 p-10 items-center">
           {
             comments.length ? comments.map((comment: any) => <Comment 
             content={comment.snippet.topLevelComment.snippet.textDisplay} 
