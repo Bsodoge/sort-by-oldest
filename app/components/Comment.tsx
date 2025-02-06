dc6359d9c7326430d64e4361a656df0845fd177d
bsodoge <kashhh4u@gmail.com>
2025-02-06 19:17:45 +0000

    we do lil css

--- app/components/Comment.tsx
+++ app/components/Comment.tsx
@@ -1,25 +1,27 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime"
+import { convert } from "html-to-text";
 import Image from "next/image";
 
 export default function Comment({ content, publishedAt, author, likes, profileImage }: any) {
     dayjs.extend(relativeTime);
     return(
-        <div className="flex gap-8 border p-4 w-1/2 max-w-3/4">
+        <div className="flex gap-8 bg-white border p-4 w-1/2 max-w-3/4 box-content">
             <div>
-                <Image width={64} height={64} src={profileImage} alt="No Profile Found" />
+                <Image width={64} height={64} src={profileImage} alt="No Profile Found"/>
             </div>
             <div className="flex flex-col gap-4 flex-1">
                 <div className="flex gap-2">
-                    <div className="">{ author }</div>
-                    <div className="">{ dayjs(publishedAt).fromNow() }</div>
+                    <div className="text-[#438bc5] hover:underline hover:cursor-pointer font-bold">{ author }</div>
+                    <div className="text-[#999]">{ dayjs(publishedAt).fromNow() }</div>
                 </div>
                 <div className="">
-                    <div className="">{ content }</div>
+                    <div className="text-[#333]">{ convert(content) }</div>
                 </div>
-                <div className="flex gap-4">
-                    <div className="">{ likes } likes</div>
-                    <div className="">replies</div>
+                <div className="flex text-[#999] gap-4">
+                    <div className="font-bold">Reply</div>
+                    <span className="font-bold">·</span>
+                    <div className="text-[#090] flex gap-2 items-center">{ likes ? likes : "" } <div className="thumbs_up icons"></div></div>
                 </div>
             </div>
         </div>
